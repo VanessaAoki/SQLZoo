7f757cdc01d9375998099158dd8b244398d0171e
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-03-19 09:21:09 -0300

    Update the join tutorial

--- Tutorials/join.sql
+++ Tutorials/join.sql
@@ -1,6 +1,11 @@
+-- https://sqlzoo.net/wiki/The_JOIN_operation
+
 /*
 1. 
 The first example shows the goal scored by a player with the last name 'Bender'. The * says to list all the columns in the table - a shorter way of saying matchid, teamid, player, gtime
+
+SELECT * FROM goal 
+WHERE player LIKE '%Bender'
 
 Modify it to show the matchid and player name for all goals scored by Germany. To identify German players, check for: teamid = 'GER'
 */
